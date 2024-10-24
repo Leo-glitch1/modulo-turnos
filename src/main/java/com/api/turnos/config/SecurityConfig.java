91a62059ee73df641cc8266e195444a49c6fd276
luis01chavez <al222010012@gmail.com>
2024-10-24 12:30:12 -0600

    Se agregan lineas para desabilitar CORS

--- src/main/java/com/api/turnos/config/SecurityConfig.java
+++ src/main/java/com/api/turnos/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebSecurity
@@ -28,4 +29,13 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/")
+                .allowedOrigins("*") // Se ajusta cuando se cambie al servidor
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 }
